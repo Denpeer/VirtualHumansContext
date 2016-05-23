2b5d9ab84310f57b64d55b8da75e8f99f092d11c
Max <Max_Groenenboom@hotmail.com>
2016-05-23 12:13:35 +0200

    Added base functionality for the agent to read indicators.

--- src/DUWOagent/knowledge.pl
+++ src/DUWOagent/knowledge.pl
@@ -3,6 +3,7 @@
 :- dynamic settings/1.
 :- dynamic functions/1.
 :- dynamic buildings/1.
+:- dynamic indicator/1.
 
 % we have a building if the building list has at least 1 element.
 havebuilding :- buildings([X|Y]).
