b3ab46a255cfda3dba1022f365e6c976330debb4
Max Groenenboom <M.Groenenboom@student.tudelft.nl>
2016-05-23 13:09:27 +0200

    Fixed wrong name of indicators belief, added comments.

--- src/DUWOagent/knowledge.pl
+++ src/DUWOagent/knowledge.pl
@@ -3,7 +3,7 @@
 :- dynamic settings/1.
 :- dynamic functions/1.
 :- dynamic buildings/1.
-:- dynamic indicator/1.
+:- dynamic indicators/1.
 
 % we have a building if the building list has at least 1 element.
 havebuilding :- buildings([X|Y]).
