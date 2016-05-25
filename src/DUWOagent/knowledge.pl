586c2fa07cff051f2d053e17b10c2f6e3acfb5e0
Rico Tubbing <rico12978@hotmail.com>
2016-05-25 16:01:43 +0200

    Added comments to knowledge.pl
    Also added a check to getIndicator to see if cur val< tar val. Also added
    a goal to build more student housing.

--- src/DUWOagent/knowledge.pl
+++ src/DUWOagent/knowledge.pl
@@ -5,10 +5,20 @@
 :- dynamic buildings/1.
 :- dynamic indicator/3.
 :- dynamic indicatorWeight/3.
+:- dynamic planned/3.
 
 % we have a building if the building list has at least 1 element.
 havebuilding :- buildings([X|Y]).
 
+
 % Get indicator
 getIndicator(Name, Weight, CurrentValue, TargetValue) :- 
 	indicatorWeight(ID,Name,Weight), indicator(ID, CurrentValue, TargetValue).
+	
+% Only build houses when our current value < target value.
+needStudentHousing:- 
+	getIndicator('Bouw DUWO', Weight, CurrentValue, TargetValue),
+	CurrentValue < TargetValue.
+	
+goalBuildStudentHousing.
+	
