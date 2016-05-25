6820e917564c563d4e54991eafc17d7d03ae3399
Rico Tubbing <rico12978@hotmail.com>
2016-05-25 10:33:14 +0200

    Insert indicators separately instead of a list.

--- src/DUWOagent/knowledge.pl
+++ src/DUWOagent/knowledge.pl
@@ -3,7 +3,7 @@
 :- dynamic settings/1.
 :- dynamic functions/1.
 :- dynamic buildings/1.
-:- dynamic indicators/1.
+:- dynamic indicator/3.
 :- dynamic indicatorWeight/3.
 
 % we have a building if the building list has at least 1 element.
