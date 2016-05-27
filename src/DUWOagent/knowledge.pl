9aea61a9f29038c2c8ecba2c0f6dc1cbb29197b6
Denpeer <D.J.P.vanPeer@student.tudelft.nl>
2016-05-27 11:12:13 +0200

    Budget modules tests added Getindicator predicate updated

--- src/DUWOagent/knowledge.pl
+++ src/DUWOagent/knowledge.pl
@@ -10,12 +10,12 @@
 
 % Get indicator
 %indicator names: "Astand TUDelft", "Bouw DUWO", "Budget DUWO", "Ruimtelijke kwaliteit", "Variatie Woonruimte"
-getIndicator(Name, Weight, CurrentValue, TargetValue) :- 
+getIndicator(Name, Weight, CurrentValue, TargetValue, ID) :- 
 	indicatorWeight(ID,Name,Weight), indicator(ID, CurrentValue, TargetValue).
 	
 % Only build houses when our current value < target value.
 needStudentHousing:- 
-	getIndicator('Bouw DUWO', Weight, CurrentValue, TargetValue),
+	getIndicator('Bouw DUWO', Weight, CurrentValue, TargetValue, ID),
 	CurrentValue < TargetValue.
 	
 goalBuildStudentHousing.
@@ -23,9 +23,9 @@ goalBuildStudentHousing.
 % Budget predicates that the bot can use to either stop building or build more carefully (raising a value needed per building for example)
 % These predicates expect DUWO to keep its target budget as a minimum (since DUWO can't raise it's budget by other means than selling property)
 lowBudget:-
-	getIndicator('Budget DUWO', Weight, CurrentValue, TargetValue),
+	getIndicator('Budget DUWO', Weight, CurrentValue, TargetValue, ID),
 	CurrentValue < 1.2*TargetValue.
 	
 noBudget:-
-	getIndicator('Budget DUWO', Weight, CurrentValue, TargetValue),
+	getIndicator('Budget DUWO', Weight, CurrentValue, TargetValue, ID),
 	CurrentValue < TargetValue.	
