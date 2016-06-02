136eb4e1d33669999c7a5bc4cb60b687b30a2477
Denpeer <D.J.P.vanPeer@student.tudelft.nl>
2016-06-02 19:50:58 +0200

    Speed update & comments

--- src/DUWOagent/predicates.pl
+++ src/DUWOagent/predicates.pl
@@ -9,10 +9,12 @@
 :- dynamic self/1.
 :- dynamic stakeholder/2.
 :- dynamic relevant_areas/2.
-:- dynamic ownedBuildings/2.
+:- dynamic nonStudentBuilding/2.
 :- dynamic cleaned / 0.
+:- dynamic refreshcounter /1.
 
-ownedBuildings(Bid,Name) :- 
+% A predicate containing a building that doesn't influence our building indicators
+nonStudentBuilding(Bid,Name) :- 
 		buildings(Y),
 		self(OwnId),
 		member(building(Bid,Name,OwnId,Year,Cat,_,_),Y),
