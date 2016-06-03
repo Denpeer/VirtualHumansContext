070ed8aed759699c7620c7d129576593c9c52611
Denpeer <D.J.P.vanPeer@student.tudelft.nl>
2016-06-03 17:42:31 +0200

    Created seperate agent to test our actions. Further testing requires issue 64 on the eishub connector to be fixed

---
+++ src/DUWOtestAgent/predicates.pl
@@ -0,0 +1,45 @@
+:- dynamic stakeholders/1.
+:- dynamic settings/1.
+:- dynamic functions/1.
+:- dynamic buildings/1.
+:- dynamic zones/1.
+:- dynamic indicator/3.
+:- dynamic indicatorWeight/3.
+:- dynamic planned/3.
+:- dynamic self/1.
+:- dynamic stakeholder/2.
+
+% Get indicator
+%indicator names: "Astand TUDelft", "Bouw DUWO", "Budget DUWO", "Ruimtelijke kwaliteit", "Variatie Woonruimte"
+getIndicator(ID, Name, Weight, CurrentValue, TargetValue) :-
+	indicatorWeight(ID,Name,Weight), indicator(ID, CurrentValue, TargetValue).
+
+% Only build houses when our current value < target value.
+needStudentHousing :-
+	getIndicator(ID,'Bouw DUWO', Weight, CurrentValue, TargetValue),
+	CurrentValue < TargetValue.
+
+goalBuildStudentHousing :-
+	needStudentHousing.
+
+% Budget predicates that the bot can use to either stop building or build more carefully (raising a value needed per building for example)
+% These predicates expect DUWO to keep its target budget as a minimum (since DUWO can't raise it's budget by other means than selling property)
+lowBudget :-
+	getIndicator(ID,'Budget DUWO', Weight, CurrentValue, TargetValue),
+	CurrentValue < 1.2*TargetValue.
+
+noBudget :-
+	getIndicator(ID,'Budget DUWO', Weight, CurrentValue, TargetValue),
+	CurrentValue < TargetValue.
+
+goalReachBudgetTarget :-
+	not(lowBudget),
+	not(noBudget).
+
+% Get a multipolygon as a sqaure with X, Y, Width and Height as coordinates.
+getPolygon(X, Y, Width, Height, Square) :-
+	XAndWidth is X+Width,
+	YAndHeight is Y+Height,
+	format(atom(A), "MULTIPOLYGON(((~w ~w, ~w ~w, ~w ~w, ~w ~w, ~w ~w)))",
+	[X,Y,XAndWidth,Y, XAndWidth, YAndHeight,X, YAndHeight, X, Y]),
+	Square = multipolygon(A).
