d48b9f5240d3d34bc80b0b4718eb02ca7b4ad095
Rico Tubbing <rico12978@hotmail.com>
2016-06-13 12:10:15 +0200

    Add comments to demolishPredicates.pl

--- src/DUWOagent/demolishPredicates.pl
+++ src/DUWOagent/demolishPredicates.pl
@@ -7,15 +7,20 @@
 :- dynamic self/1.
 :- dynamic buildings/1.
  
+ % Get a buildings id and area by catergory.
 getAreaOwnBuilding(BuildingID, Category, Area):- 
 	self(OwnID),
 	buildings(AllBuildings),
 	member(building(BuildingID,_Name,OwnID,_Year,Cat,_,_,_Poly,Area),AllBuildings),
 	member(Category, Cat).
 
+% Create a list with the id and area of all buildings
+% In the form of:
+% [[BuildingIdD,Area1],[BuildingID2,Area2], ...]
 allBuildings(Category, List):-
 	findall([BuildingID,Area],getAreaOwnBuilding(BuildingID, Category, Area),List). 
  
+% Find the maximum area.
 maxArea([[BuildingID,Area]],BuildingID, Area).
 maxArea([[_BuildingID,Area]|RestList], BuildingIDFromMax, Max):-
 	maxArea(RestList, BuildingIDFromMax, Max),
